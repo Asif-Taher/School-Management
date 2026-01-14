2d324ba4de3f682dcfd3acff2d970c67a6bc58db
Asif Taher <mdasiftaher2020@gmail.com>
2026-01-14 17:49:22 +0600

    add new line

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,9 +1,24 @@
+import CountChart from '@/components/CountChart'
+import UserCart from '@/components/UserCart'
 import React from 'react'
 
 const AdminPage = () => {
   return (
-    <div>
-      <h2>This is the admin page</h2>
+    <div className='p-4 flex flex-col md:flex-row gap-4'>
+    {/* left */}
+      <div className='w-full lg:w-2/3'>
+      <div className='flex flex-wrap w-full justify-between gap-4'>
+       <UserCart type='student'/>
+      <UserCart type='teacher'/>
+      <UserCart type='parent'/>
+      <UserCart type='admin'/>
+      </div>
+      <CountChart />
+      </div>
+      {/* right */}
+      <div className='w-full lg:w-1/3'>
+      <h2>Right</h2>
+      </div>
     </div>
   )
 }
---
+++ src/components/CountChart.tsx
@@ -0,0 +1,95 @@
+"use client";
+import React from 'react'
+import { RadialBarChart, RadialBar, Legend, Tooltip } from 'recharts';
+
+import Image from 'next/image';
+// #region Sample data
+const data = [
+  {
+    name: '18-24',
+    uv: 31.47,
+    pv: 2400,
+    fill: '#8884d8',
+  },
+  {
+    name: '25-29',
+    uv: 26.69,
+    pv: 4567,
+    fill: '#83a6ed',
+  },
+  {
+    name: '30-34',
+    uv: 15.69,
+    pv: 1398,
+    fill: '#8dd1e1',
+  },
+  {
+    name: '35-39',
+    uv: 8.22,
+    pv: 9800,
+    fill: '#82ca9d',
+  },
+  {
+    name: '40-49',
+    uv: 8.63,
+    pv: 3908,
+    fill: '#a4de6c',
+  },
+  {
+    name: '50+',
+    uv: 2.63,
+    pv: 4800,
+    fill: '#d0ed57',
+  },
+  {
+    name: 'unknown',
+    uv: 6.67,
+    pv: 4800,
+    fill: '#ffc658',
+  },
+];
+
+const CountChart = () => {
+  return (
+    <div>
+        {/* title */}
+      <div className='flex justify-between items-center'>
+       <h2>Student</h2>
+       <Image src="/moreDark.png" alt='moredark' width={20} height={20} />
+      </div>
+      {/* user chart */}
+      <div>
+        <RadialBarChart
+      style={{ width: '100%', maxWidth: '700px', maxHeight: '80vh', aspectRatio: 1.618 }}
+      responsive
+      cx="30%"
+      barSize={14}
+      data={data}
+    >
+      <RadialBar label={{ position: 'insideStart', fill: '#fff' }} background dataKey="uv" />
+      <Legend iconSize={10} layout="vertical" verticalAlign="middle"/>
+      <Tooltip />
+    </RadialBarChart>
+      </div>
+      {/* middle chart */}
+      <div>
+        {/* count chart */}
+        <div>
+
+        </div>
+        {/* attandence chart */}
+        <div>
+
+        </div>
+      </div>
+      {/* bottom chart */}
+      <div>
+
+      </div>
+      {/* bottom */}
+      <div></div>
+    </div>
+  )
+}
+
+export default CountChart
